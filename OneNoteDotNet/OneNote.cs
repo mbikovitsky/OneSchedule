50d6791d86f7b0fd94fa8296502b9639674d7c13
Michael Bikovitsky <biko@bikodbg.com>
2021-04-24 18:20:01 +0300

    Implement retrieving OneNote page content

--- OneNoteDotNet/OneNote.cs
+++ OneNoteDotNet/OneNote.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Linq;
+﻿using System;
+using System.Xml.Linq;
 using Microsoft.Office.Interop.OneNote;
 
 namespace OneNoteDotNet
@@ -19,6 +20,32 @@ namespace OneNoteDotNet
                 _application.GetHierarchy(null, HierarchyScope.hsPages, out var xmlString, XMLSchema.xs2013);
                 var xml = XElement.Parse(xmlString);
                 return new Hierarchy(xml);
+            }
+        }
+
+        public PageContent GetPageContent(string id, PageInfo pageInfo)
+        {
+            var mappedPagedInfo = MapPageInfo(pageInfo);
+            _application.GetPageContent(id, out var xmlString, mappedPagedInfo, XMLSchema.xs2013);
+            var xml = XElement.Parse(xmlString);
+            return new PageContent(xml);
+        }
+
+        private static Microsoft.Office.Interop.OneNote.PageInfo MapPageInfo(PageInfo pageInfo)
+        {
+            switch (pageInfo)
+            {
+                case PageInfo.Basic:
+                case PageInfo.BinaryData:
+                case PageInfo.Selection:
+                case PageInfo.BinaryDataSelection:
+                case PageInfo.FileType:
+                case PageInfo.BinaryDataFileType:
+                case PageInfo.SelectionFileType:
+                case PageInfo.All:
+                    return (Microsoft.Office.Interop.OneNote.PageInfo) pageInfo;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(pageInfo), pageInfo, null);
             }
         }
     }
---
+++ OneNoteDotNet/PageContent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+using HtmlAgilityPack;
+
+namespace OneNoteDotNet
+{
+    public class PageContent
+    {
+        public PageContent(XElement xml)
+        {
+            Xml = xml;
+        }
+
+        public XElement Xml { get; }
+
+        public IEnumerable<string> TextElements => Xml.Descendants(Xml.Name.Namespace + "T").Select(element => element.Value);
+
+        public IEnumerable<string> PlainTextElements => TextElements.Select(text =>
+        {
+            var html = new HtmlDocument();
+            html.LoadHtml(text);
+            var encodedText = html.DocumentNode.InnerText.Trim();
+            var decodedText = HttpUtility.HtmlDecode(encodedText);
+            return decodedText;
+        });
+    }
+}
