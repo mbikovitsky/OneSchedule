bb02f59c78cc0387b2252eae05e9ea7d66991ad2
Michael Bikovitsky <biko@bikodbg.com>
2021-04-27 00:38:39 +0300

    Create project

---
+++ OneExec/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OneExec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
